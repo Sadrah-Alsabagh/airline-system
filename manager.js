d703b1969593d883ef626dbd6c868504c7651305
Sadrah-Alsabagh <sedrasabbagh33@gmail.com>
2022-11-01 21:50:04 +0000

    lab 19

---
+++ manager.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const events = require( './events.js' );
+require( './system.js' );
+require( './pilot.js' );
+const { faker } = require('@faker-js/faker');
+const { v4: uuidv4 } = require('uuid');
+ 
+setInterval(()=>{
+    // const {country,city} = faker.address;
+    const info={
+        airline:'Jordan Royal Airlines',
+        flightID:uuidv4(),
+        pilot:faker.name.fullName(),
+        destination:`${faker.address.cityName(), faker.address.country()}`,
+    };
+    console.log(`Manager: new flight with ID '${info.flightID}'have been scheduled`);
+    events.emit('new-flight',info);},10000);
+
+events.on('arrived',(info)=>{console.log( `Manager: we\’re greatly thankful for the amazing flight, ${info.pilot}`)});
---
+++ system.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const events = require( './events.js' );
+require( './manager.js' );
+require( './pilot.js' );
+
+
+events.on('new-flight',handleNewFlight);
+
+function handleNewFlight(info){
+    console.log(`Flight {
+        event:'new-flight',
+        time: ${new Date().toLocaleString()},
+        Details: 
+        {
+            airline:'${info.airline}',
+            flightID:'${info.flightID}',
+            pilot:'${info.pilot}',
+            destination:'${info.destination}',
+        }
+    }`);}
+
+events.on('took-off',handleTookOff);
+function handleTookOff(info){
+    console.log(`Flight {
+        event:'took-off',
+        time:${new Date().toLocaleString()},
+        Details: 
+        {
+            airline:'${info.airline}',
+            flightID:'${info.flightID}',
+            pilot:'${info.pilot}',
+            destination:'${info.destination}',
+        }
+    }`);}
+
+events.on( 'arrived',handleArrived);
+function handleArrived(info){
+    console.log(`Flight {
+        event:'arrived',
+        time:${new Date().toLocaleString()},
+        Details:
+        {
+            airline:'${info.airline}',
+            flightID:'${info.flightID}',
+            pilot:'${info.pilot}',
+            destination:'${info.destination}',
+        }
+    }`);}
